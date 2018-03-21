c8247e64721f56b5b637154c5668ed8fcee4d5e3
jane martin <223120023@student.unaki.ac.id>
2018-03-21 02:20:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,66 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |winCombinationarray|
+    winner=[]
+    oindex=0
+    xindex=0
+    winCombinationarray.each do |winCombinationEach|
+      if board[winCombinationEach]=="X"&&oindex==0
+        xindex+=1
+        winner<<winCombinationEach
+      elsif board[winCombinationEach]=="O"&&xindex==0
+        oindex+=1
+        winner<<winCombinationEach
+      end
+    end
+    if xindex==3||oindex==3
+      return winner
+    end
+  end
+  return false
+end
+
+def full?(board)
+  full=true
+  board.each do |board|
+    if draw?(board)
+      full= true
+    elsif board==""||board==" "||board==nil
+      full= false
+    end
+  end
+  return full
+end
+
+def draw?(board)
+  !won?(board)
+end
+def over?(board)
+  if won?(board)||draw?(board)
+    return true
+  end
+end
+def winner(board)
+  won?(board).each do |theWinner|
+    if board[theWinner]=="X"
+      return "X"
+    elsif board[theWinner]=="O"
+      return "O"
+    else
+      return nil
+    end
+  end
+end
